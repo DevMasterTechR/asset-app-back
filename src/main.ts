28356de8a2df3835c44d43fd71876e57f831f2e5
DevTech2code <genericosistem2@gmail.com>
2026-01-15 15:33:58 -0500

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -41,7 +41,7 @@ async function bootstrap() {
     'http://192.168.50.95:8080',
     'http://localhost:8080',
     'http://localhost:5173',
-    'https://asset-app-front.vercel.app',
+    'https://amazing-starburst-1ad910.netlify.app',
   ],
     credentials: true,
   });
