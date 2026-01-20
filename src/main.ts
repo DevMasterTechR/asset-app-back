2438ae7d73bf25954db8bb63a52893412a7c20d9
DevTech2code <genericosistem2@gmail.com>
2026-01-20 15:33:28 -0500

    sdasdasd

--- src/main.ts
+++ src/main.ts
@@ -41,7 +41,7 @@ async function bootstrap() {
     'http://192.168.50.95:8080',
     'http://localhost:8080',
     'http://localhost:5173',
-    'https://steady-crumble-eb147b.netlify.app'
+    'https://asset-app-front.vercel.app/'
   ],
     credentials: true,
   });
